f081d1ecb1da8f05ab45a48dad784dbab1853b0a
SalahAlDien <151957291+SalahAlDien@users.noreply.github.com>
2023-12-06 14:44:40 +0200

    Update Journal.java

--- Library-System/src/main/java/com/mycompany/library/system/Journal.java
+++ Library-System/src/main/java/com/mycompany/library/system/Journal.java
@@ -5,9 +5,9 @@ class Journal extends Book {
     private String conferenceName;
     private int conferenceNo;
 
-    public Journal(String title, Author author, String no, String genre, int version, Date date,
+    public Journal(String title, String author, String no, String genre, int version, Date date,
                    String conferenceName, int conferenceNo) {
-        super(title, author, no, genre, version, date);
+        super(title, author, no, genre, version);
         this.conferenceName = conferenceName;
         this.conferenceNo = conferenceNo;
     }
